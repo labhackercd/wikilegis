7cd46130251f075509e9762f3355fc3ac28f37fc
Dirley Rodrigues <dirleyrls@gmail.com>
2015-09-03 13:43:12 -0300

    Turn the logo into a clickable link
    That, as you guessed it, gets the user to the front page of the system.

--- wikilegis/core/urls.py
+++ wikilegis/core/urls.py
@@ -1,7 +1,7 @@
 from django.conf.urls import url
 
 urlpatterns = [
-    url(r'^$', 'wikilegis.core.views.index'),
+    url(r'^$', 'wikilegis.core.views.index', name='index'),
     url(r'^bill/(?P<bill_id>\d+)/$', 'wikilegis.core.views.show_bill', name='show_bill'),
     url(r'^bill/(?P<bill_id>\d+)/segments/(?P<segment_id>\d+)/$', 'wikilegis.core.views.show_segment', name='show_segment'),
 ]

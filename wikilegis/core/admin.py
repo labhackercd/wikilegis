b686487bd0737b0c4901f89a33b978d6c7337191
Dirley Rodrigues <dirleyrls@gmail.com>
2015-09-04 14:29:18 -0300

    Fix potential encoding problems in models.
    Plus, some styling changes.

--- wikilegis/core/admin.py
+++ wikilegis/core/admin.py
@@ -2,16 +2,20 @@ from django.contrib import admin
 from adminsortable2.admin import SortableInlineAdminMixin
 from . import models
 
-class BillSegmentInline(SortableInlineAdminMixin, admin.TabularInline):  # or admin.StackedInline
+
+class BillSegmentInline(SortableInlineAdminMixin, admin.TabularInline):
     model = models.BillSegment
 
+
 class BillAdmin(admin.ModelAdmin):
-    list_display = ('title', 'description',)
+    list_display = ('title', 'description')
     inlines = (BillSegmentInline,)
+
 
 class BillSegmentAdmin(admin.ModelAdmin):
     list_display = ('type', 'content')
     list_filter = ('bill',)
+
 
 class CitizenAmendmentAdmin(admin.ModelAdmin):
     list_display = ('author', 'segment', 'original_content', 'content', 'comment')
--- wikilegis/core/models.py
+++ wikilegis/core/models.py
@@ -8,7 +8,7 @@ class Bill(models.Model):
     title = models.CharField(max_length=255)
     description = models.TextField()
 
-    def __str__(self):
+    def __unicode__(self):
         return self.title
 
 
@@ -18,16 +18,16 @@ class BillSegment(models.Model):
     content = models.TextField()
 
     TYPE_CHOICES = (
-        ('title', _('Title')),
-        ('article', _('Article')),
+        ('title', _(u'Title')),
+        ('article', _(u'Article')),
     )
     type = models.CharField(max_length=64, choices=TYPE_CHOICES)
 
     class Meta:
         ordering = ('order',)
 
-    def __str__(self):
-        return '%s: %s' % (self.bill, self.content, )
+    def __unicode__(self):
+        return u'%s: %s' % (self.bill, self.content)
 
     def is_editable(self):
         # Currently, only articles are editable.
@@ -36,7 +36,7 @@ class BillSegment(models.Model):
 
 class CitizenAmendment(models.Model):
     author = models.ForeignKey(settings.AUTH_USER_MODEL)
-    segment = models.ForeignKey(BillSegment, related_name='amendments')
+    segment = models.ForeignKey('core.BillSegment', related_name='amendments')
     content = models.TextField()
     comment = models.TextField()
 

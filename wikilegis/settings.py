db144d4c59de9dc01387f5180688996d90c02363
Dirley Rodrigues <dirleyrls@gmail.com>
2015-09-23 18:40:57 +0000

    Fix static files

--- wikilegis/settings.py
+++ wikilegis/settings.py
@@ -169,7 +169,7 @@ STATICFILES_FINDERS = default.STATICFILES_FINDERS + (
 # Simplified static file serving.
 # https://warehouse.python.org/project/whitenoise/
 
-STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.ManifestStaticFilesStorage'
 
 COMPRESS_PRECOMPILERS = (
     ('text/x-scss', 'django_libsass.SassCompiler'),

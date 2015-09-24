fa109b9768412f8abe58202de42e581c57200c86
Dirley Rodrigues <dirleyrls@gmail.com>
2015-09-24 10:30:10 -0300

    Redirect after logout

--- wikilegis/auth2/helpers.py
+++ wikilegis/auth2/helpers.py
@@ -3,6 +3,7 @@ from __future__ import unicode_literals
 from urlparse import urlparse, urlunparse
 from django.conf import settings
 from django.contrib.auth import REDIRECT_FIELD_NAME
+from django.core.urlresolvers import reverse
 from django.http import QueryDict
 from django.shortcuts import resolve_url
 from wikilegis.helpers import nocontext
@@ -19,14 +20,27 @@ def login_absolute_path(request, login_url=None, redirect_field_name=REDIRECT_FI
     if ((not login_scheme or login_scheme == current_scheme) and
             (not login_netloc or login_netloc == current_netloc)):
         path = request.get_full_path()
-
     resolved_url = resolve_url(login_url or settings.LOGIN_URL)
-
     login_url_parts = list(urlparse(resolved_url))
     if redirect_field_name:
         querystring = QueryDict(login_url_parts[4], mutable=True)
         querystring[redirect_field_name] = path
         login_url_parts[4] = querystring.urlencode(safe='/')
-
     return urlunparse(login_url_parts)
 
+
+@nocontext
+def logout_absolute_path(request, next_page_field_name=REDIRECT_FIELD_NAME):
+    path = request.build_absolute_uri()
+    resolved_url = reverse('auth_logout')
+    scheme, netloc = urlparse(resolved_url)[:2]
+    current_scheme, current_netloc = urlparse(path)[:2]
+    if ((not scheme or scheme == current_scheme) and
+            (not netloc or netloc == current_netloc)):
+        path = request.get_full_path()
+    logout_url_parse = list(urlparse(resolved_url))
+    if next_page_field_name:
+        querystring = QueryDict(logout_url_parse[4], mutable=True)
+        querystring[next_page_field_name] = path
+        logout_url_parse[4] = querystring.urlencode(safe='/')
+    return urlunparse(logout_url_parse)

00b281bb8e2712ffd8816c51ac5a263accafac94
Dirley Rodrigues <dirleyrls@gmail.com>
2015-09-29 09:29:54 -0300

    Little tweaks to registration process
    * Redirect the user back to home after activation. With an info message.
    
    * Display activation message within our base template.
    
    This is not ideal, tho. We should redirect the user "back to where he came
    from" after registration is complete. But we can't do that now, because we
    have no reliable way to show flash messages to the user (since our flash
    messages currently suck and don't even work).
    
    In order to accomplish this, tho, we would have to implement the `next` field
    in the registration view, then save it in the registration profile, then
    redirect the user if he come quick enough. It's a real mess and I can't deal
    with it right now.

--- wikilegis/auth2/urls.py
+++ wikilegis/auth2/urls.py
@@ -2,8 +2,15 @@ from django.conf import settings
 from django.conf.urls import url, include
 from registration.backends.default.views import RegistrationView
 from registration.backends.simple.views import RegistrationView as SimpleRegistrationView
+from wikilegis.auth2.views import ActivationCompleteView
 
 urlpatterns = [
+    # XXX We want the user to be redirected after successful account activation.
+    # So we override the `registration_activation_complete` view to do the redirect.
+    # Note that tt overrides the included view from `registration.backends.default`
+    # and should always come before its inclusion or else little cutie puppies will cry.
+    url(r'^activate/complete/$', ActivationCompleteView.as_view(),
+        name='registration_activation_complete'),
     url(r'^', include('registration.backends.default.urls')),
 ]
 

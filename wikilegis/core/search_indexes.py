fa34b905a551281318bbec1374a86d4a69a4b89b
Dirley Rodrigues <dirleyrls@gmail.com>
2015-10-05 15:09:14 -0300

    Indent with spaces instead of tabs

--- wikilegis/core/search_indexes.py
+++ wikilegis/core/search_indexes.py
@@ -4,17 +4,17 @@ from wikilegis.core.models import Bill
 
 
 class BillIndex(indexes.SearchIndex, indexes.Indexable):
-	text = indexes.CharField(document=True)
+    text = indexes.CharField(document=True)
 
 
-	def get_model(self):
-		return Bill
+    def get_model(self):
+        return Bill
 
-	def index_queryset(self, using=None):
-		return self.get_model().objects.all()
+    def index_queryset(self, using=None):
+        return self.get_model().objects.all()
 
-	def prepare_text(self, obj):
-		content = obj.title + '\n\n'
-		content += obj.description + '\n\n'
-		content += obj.content
-		return content
+    def prepare_text(self, obj):
+        content = obj.title + '\n\n'
+        content += obj.description + '\n\n'
+        content += obj.content
+        return content

504fc050bf4218ceb699d8a09d6b15845da307e8
Dirley Rodrigues <dirleyrls@gmail.com>
2015-10-14 15:18:22 -0300

    Fix unicode error in the admin

--- wikilegis/core/admin.py
+++ wikilegis/core/admin.py
@@ -4,7 +4,7 @@ from django.contrib import admin
 from django.contrib.admin.views.main import ChangeList
 from django.contrib.auth import get_permission_codename
 from django.contrib.contenttypes.admin import GenericTabularInline
-from django.utils.translation import gettext_lazy as _
+from django.utils.translation import ugettext_lazy as _
 from adminsortable2.admin import SortableInlineAdminMixin
 from . import models, forms
 

37a9f46969595573fd215a5386abb6eaff1fb087
Dirley Rodrigues <dirleyrls@gmail.com>
2015-10-15 15:17:07 -0300

    Fix facebook login [2]
    We also need to explicitly request the email scope

--- wikilegis/settings.py
+++ wikilegis/settings.py
@@ -177,8 +177,10 @@ SOCIAL_AUTH_GOOGLE_OAUTH2_SECRET = ''
 SOCIAL_AUTH_FACEBOOK_KEY = ''
 SOCIAL_AUTH_FACEBOOK_SECRET = ''
 
+SOCIAL_AUTH_FACEBOOK_SCOPE = ['email']
+
 SOCIAL_AUTH_FACEBOOK_PROFILE_EXTRA_PARAMS = {
-    'fields': 'id, name, email'
+    'fields': 'id,name,first_name,last_name,email'
 }
 
 

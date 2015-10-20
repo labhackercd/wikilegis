6919baa79e7a0f0b8ccd21d4b8026d7887f3a737
Erivanio Vasconcelos <erivanio.vanin@gmail.com>
2015-10-20 14:27:20 -0200

    create email notification
    send a daily notification to admins and editors when an amendment or a comment is created

--- wikilegis/notification/admin.py
+++ wikilegis/notification/admin.py
@@ -3,8 +3,12 @@ from wikilegis.notification.models import HistoryNotification
 
 
 class HistoryNotificationAdmin(admin.ModelAdmin):
-    list_display = ('bill', 'hour')
-    list_filter = ['bill', 'hour']
-    search_fields = ['bill']
+    list_display = ('related_segment', 'hour')
+    list_filter = ['amendment__segment__bill', 'amendment__segment', 'hour']
+    search_fields = ['amendment__segment', 'amendment__segment__bill']
+
+    def related_segment(self, obj):
+        return obj.amendment.segment
+    related_segment.short_description = 'Segment'
 
 admin.site.register(HistoryNotification, HistoryNotificationAdmin)
--- wikilegis/notification/management/commands/send_mail.py
+++ wikilegis/notification/management/commands/send_mail.py
@@ -1,44 +1,61 @@
 # -*- coding: utf-8 -*-
+from collections import defaultdict
 from datetime import datetime
+from django.contrib.contenttypes.models import ContentType
+from django.contrib.sites.models import Site
 from django.core.management.base import BaseCommand
 from django.core.mail import EmailMultiAlternatives
 from django.template.loader import render_to_string
+from django_comments.models import Comment
 from wikilegis.auth2.models import User
-from wikilegis.core.models import Bill, BillSegment
+from wikilegis.core.models import Bill, CitizenAmendment
 from wikilegis.notification.models import HistoryNotification
 
 
 class Command(BaseCommand):
     def handle(self, *args, **options):
         bills = Bill.objects.all()
-
+        current_site = Site.objects.get_current()
+        import ipdb; ipdb.set_trace()
         for bill in bills:
-            try:
-                last_email = HistoryNotification.objects.filter(bill=bill).order_by('-hour')[0]
-            except:
-                last_email = ''
-
-            if last_email:
-                segments = BillSegment.objects.filter(bill=bill, modified__gt=last_email).order_by('-modified')
-            else:
-                segments = BillSegment.objects.filter(bill=bill).order_by('-modified')
-
-            html = render_to_string('notification/notification_email.html', {'segments': segments})
-            superusers = User.objects.filter(is_superuser=True)
-            email_list = list()
-
-            for superuser in superusers:
-                email_list.append(superuser.email)
-            for editor in bill.editors.all():
-                for user in editor.user_set.all():
-                    email_list.append(user.email)
-
-            mail = EmailMultiAlternatives('Notificações - Wikilegis', '', 'erivanio.vasconcelos@gmail.com', email_list)
-            mail.attach_alternative(html, 'text/html')
-            sent = mail.send()
-
-            if sent:
-                history = HistoryNotification()
-                history.bill = bill
-                history.hour = datetime.now()
-                history.save()
+            segment_amendments = defaultdict(list)
+            amendment_comments = defaultdict(list)
+            for segment in bill.segments.all():
+                for amendment in segment.amendments.all():
+                    try:
+                        last_email = HistoryNotification.objects.get(amendment=amendment)
+                        comments = Comment.objects.filter(object_pk=amendment.pk,
+                                                          content_type=ContentType.objects.get_for_model(CitizenAmendment),
+                                                          submit_date__gte=last_email.hour)
+                        if comments:
+                            for comment in comments:
+                                amendment_comments[amendment.content].append(comment.comment)
+                            last_email.hour = datetime.now()
+                            last_email.save()
+                    except HistoryNotification.DoesNotExist:
+                        segment_amendments[segment.content].append(amendment)
+                        comments = Comment.objects.filter(object_pk=amendment.pk,
+                                                          content_type=ContentType.objects.get_for_model(CitizenAmendment))
+                        history = HistoryNotification()
+                        history.amendment = amendment
+                        history.hour = datetime.now()
+                        if comments:
+                            for comment in comments:
+                                amendment_comments[amendment.content].append(comment.comment)
+                                history.hour = datetime.now()
+                        history.save()
+            if segment_amendments or amendment_comments:
+                html = render_to_string('notification/notification_email.html',
+                                        {'current_site': current_site, 'bill': bill.title,
+                                         'segments': bill.segments.values_list('id', flat=True),
+                                         'amendments': dict(segment_amendments), 'comments': dict(amendment_comments)})
+                superusers = User.objects.filter(is_superuser=True)
+                email_list = []
+                for superuser in superusers:
+                    email_list.append(superuser.email)
+                for editor in bill.editors.all():
+                    for user in editor.user_set.all():
+                        email_list.append(user.email)
+                mail = EmailMultiAlternatives('Notificações - Wikilegis', '', 'erivanio.vasconcelos@gmail.com', email_list)
+                mail.attach_alternative(html, 'text/html')
+                mail.send()

a1f545dd725afbb5c3374a6443a59786bff8f0ed
Erivanio Vasconcelos <erivanio.vanin@gmail.com>
2015-10-20 14:27:20 -0200

    create email notification
    send a daily notification to admins and editors when an amendment or a comment is created

--- wikilegis/notification/models.py
+++ wikilegis/notification/models.py
@@ -1,11 +1,10 @@
 from datetime import datetime
 from django.db import models
 from django.utils.translation import ugettext_lazy as _
-from django.utils.text import Truncator
 
 
 class HistoryNotification(models.Model):
-    bill = models.ForeignKey('core.Bill', verbose_name=_('bill'))
+    amendment = models.ForeignKey('core.CitizenAmendment', verbose_name=_('amendment'))
     hour = models.DateTimeField(_('hour'), default=datetime.now)
 
     class Meta:
@@ -14,5 +13,4 @@ class HistoryNotification(models.Model):
         verbose_name_plural = _('history notifications')
 
     def __unicode__(self):
-        return '{bill}: {content}'.format(
-            bill=self.bill, content=Truncator(self.content).chars(100))
+        return self.amendment.segment.bill.title

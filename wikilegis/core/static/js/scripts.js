fe679a63bf309df908f34fbab089121e2fbd3f61
Marcos Felipe <marcos.fel93@gmail.com>
2015-10-21 15:56:41 -0200

    Fixed issue when have more than one proposals
    change color in the message

--- wikilegis/core/static/js/scripts.js
+++ wikilegis/core/static/js/scripts.js
@@ -1,15 +1,17 @@
 jQuery(document).ready(function($) {
+    //Collapsable-comments
     $('.collapsible-comments').attr('data-show', function(event, qtd_to_show){
-        var comments = $('.collapsible-comments-item');
+        var comments = $(this).find('.collapsible-comments-item');
 
         function hideComments(commentsList, maxComments){
             for (var i = commentsList.length - 1; i >= maxComments; i--) {
                 $(commentsList[i]).hide();
             };
+            
+            $(commentsList[qtd_to_show - 1]).after('<a class="show-more" href="#" title="expand to show all comments on this post" onclick>show <b>'+ String(comments.length - qtd_to_show) +'</b> more comments</a>')
         }
 
         function showMoreComments(commentsList){
-            $('.show-more').hide()
             for(var i = 0; commentsList.length > i; i++){
                 if ($(commentsList[i]).css('display') == 'none') {
                     $(commentsList[i]).show();
@@ -19,13 +21,11 @@ jQuery(document).ready(function($) {
 
         if (comments.length > qtd_to_show) {
             hideComments(comments, qtd_to_show);
-            $(comments[qtd_to_show - 1]).after('<a class="show-more" href="#" title="expand to show all comments on this post" onclick>show <b>'+ String(comments.length - qtd_to_show) +'</b> more comments</a>')
         };
         
-        $('.show-more').click(function(event) {
-            showMoreComments(comments)
-            $(this).hide()
+        $(this).find('.show-more').click(function(event) {
+            $(this).hide();
+            showMoreComments(comments);
         });
     })
-
 });

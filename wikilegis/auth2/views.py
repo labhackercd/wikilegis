4b7ac9893d25879525be34a693b13151a793d670
Marcos Felipe <marcos.fel93@gmail.com>
2015-10-27 11:58:05 -0200

    Page edit profile is working now
    - Cropping element added to images
    - The path "accounts/edit/" access the page

--- wikilegis/auth2/views.py
+++ wikilegis/auth2/views.py
@@ -1,8 +1,15 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
+
 from django.contrib import messages
 from django.utils.translation import ugettext
 from django.views.generic import RedirectView
+from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+from django.http import HttpResponse
+
+from .forms import UserForm
+from wikilegis.auth2.models import User
 
 
 class ActivationCompleteView(RedirectView):
@@ -15,3 +22,19 @@ class ActivationCompleteView(RedirectView):
     def get(self, request, *args, **kwargs):
         messages.info(request, ugettext("Your account is now activated."))
         return super(ActivationCompleteView, self).get(request, *args, **kwargs)
+
+
+@login_required
+def edit_profile(request):
+    user = User.objects.get(pk=request.user.pk)
+
+    if request.method == 'POST':
+        form = UserForm(request.POST, request.FILES, instance=user)
+        if form.is_valid():
+            form.save()
+            return render(request, 'registration/edit.html/', {'form': form})
+        else:
+            return HttpResponse("This is not working...")
+    else:
+        form = UserForm(instance=user)
+        return render(request, 'registration/edit.html/', {'form': form})

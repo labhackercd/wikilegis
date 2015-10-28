f43bf694026f19c42e596a758e3f6198387f1554
Marcos Felipe <marcos.fel93@gmail.com>
2015-10-28 14:30:12 -0200

    Edit form for better viewing
    - add link in some template to access the edit profile page
    - move edit.html to the right place

--- wikilegis/auth2/forms.py
+++ wikilegis/auth2/forms.py
@@ -1,5 +1,6 @@
 from django.contrib.auth.forms import UserCreationForm as BaseUserCreationForm, UserChangeForm as BaseUserChangeForm
 from image_cropping import ImageCropWidget
+from image_cropping.widgets import CropWidget
 from wikilegis.auth2.models import User
 
 
@@ -19,6 +20,18 @@ class RegistrationForm(UserCreationForm):
         fields = ('email', 'first_name', 'last_name')
 
 
+class CustomImageCropWidget(ImageCropWidget):
+    """
+    Custom ImageCropWidget that doesn't show the initial value of the field.
+    We use this trick, and place it right under the CropWidget so that
+    it looks like the user is seeing the image and clearing the image.
+    """
+    template_with_initial = (
+        # '%(initial_text)s: <a href="%(initial_url)s">%(initial)s</a> '
+        '%(clear_template)s<br />%(input_text)s: %(input)s'
+    )
+
+
 class UserForm(UserChangeForm):
     # We don't have a password field
     password = None
@@ -27,5 +40,6 @@ class UserForm(UserChangeForm):
         model = User
         fields = ('first_name', 'last_name', 'avatar', 'cropping')
         widgets = {
-            'cropping': ImageCropWidget(),
+            'avatar': CustomImageCropWidget(),
+            'cropping': CropWidget(),
         }

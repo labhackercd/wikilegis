c3df99367a8ca68655a8720371e985a9ef84d7b2
Marcos Felipe <marcos.fel93@gmail.com>
2015-11-12 13:47:38 -0200

    Fix EMAIL_HOST config

--- wikilegis/settings.py
+++ wikilegis/settings.py
@@ -267,9 +267,8 @@ THUMBNAIL_PROCESSORS = (
     'image_cropping.thumbnail_processors.crop_corners',
 ) + thumbnail_settings.THUMBNAIL_PROCESSORS
 
-EMAIL_HOST = config('EMAIL_HOST')
-EMAIL_HOST_USER = config('EMAIL_USER')
-EMAIL_HOST_PASSWORD = config('EMAIL_PWD')
-EMAIL_SUBJECT_PREFIX = config('EMAIL_SUBJECT_PREFIX')
-EMAIL_USE_TLS = config('EMAIL_TLS', cast=bool)
-EMAIL_PORT = config('EMAIL_PORT', cast=int)
+EMAIL_HOST = config('EMAIL_HOST', default='localhost')
+EMAIL_PORT = config('EMAIL_PORT', cast=int, default=587)
+EMAIL_HOST_USER = config('EMAIL_HOST_USER', default='')
+EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD', default='')
+EMAIL_USE_TLS = config('EMAIL_USE_TLS', cast=bool, default=True)

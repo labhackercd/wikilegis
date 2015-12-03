0abaedb354a1eac564fb249c3b29e59fde5c7aaf
Erivânio Vasconcelos <erivanio.vanin@gmail.com>
2015-12-03 16:30:16 -0200

    add count bill attendees in index

--- wikilegis/core/templatetags/proposals.py
+++ wikilegis/core/templatetags/proposals.py
@@ -1,7 +1,11 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 from __future__ import absolute_import
+
+from django.contrib.contenttypes.models import ContentType
 from django.template import Library
+from django_comments.models import Comment
+from wikilegis.core.models import BillSegment, CitizenAmendment, UpDownVote
 
 register = Library()
 
@@ -11,3 +15,22 @@ def proposals_count(bill):
     for segment in bill.segments.all():
         total_proposals += segment.amendments.all().count()
     return total_proposals
+
+@register.simple_tag
+def attendees_count(bill):
+    attendees = []
+    segment_ctype = ContentType.objects.get_for_model(BillSegment)
+    amendment_ctype = ContentType.objects.get_for_model(CitizenAmendment)
+    for segment in bill.segments.all():
+        for vote_segment in UpDownVote.objects.filter(content_type=segment_ctype, object_id=segment.id):
+            attendees.append(vote_segment.user.id)
+        for comment in Comment.objects.filter(object_pk=segment.pk, content_type=segment_ctype):
+            attendees.append(comment.user.id)
+        for amendment in segment.amendments.all():
+            attendees.append(amendment.author.id)
+            for vote_amendment in UpDownVote.objects.filter(content_type=amendment_ctype, object_id=amendment.id):
+                attendees.append(vote_amendment.user.id)
+            for comment in Comment.objects.filter(object_pk=amendment.pk, content_type=amendment_ctype):
+                attendees.append(comment.user.id)
+    total_attendees = len(list(set(attendees)))
+    return total_attendees

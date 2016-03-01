e89fe675f1d910da924ee519f611943cc0e2f92a
Erivanio Vasconcelos <erivanio.vanin@gmail.com>
2016-03-01 15:39:15 -0300

    alter number of proposals to contributions in bill list

--- wikilegis/core/templatetags/proposals.py
+++ wikilegis/core/templatetags/proposals.py
@@ -9,12 +9,25 @@ from wikilegis.core.models import BillSegment, UpDownVote
 
 register = Library()
 
+
 @register.filter
 def proposals_count(bill):
     total_proposals = 0
     for segment in bill.segments.filter(original=True):
         total_proposals += segment.substitutes.all().count()
     return total_proposals
+
+
+@register.filter
+def contribution_count(bill):
+    total_comments = 0
+    total_votes = 0
+    for segment in bill.segments.all():
+        total_comments += segment.comments.count()
+        total_votes += segment.votes.count()
+    total_proposals = proposals_count(bill)
+    return total_comments + total_votes + total_proposals
+
 
 @register.filter
 def attendees_count(bill):

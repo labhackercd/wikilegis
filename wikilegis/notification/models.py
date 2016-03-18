d72bd8680ea054f9480b9af615a1b0e6ef0683ef
Dirley Rodrigues <dirleyrls@gmail.com>
2016-03-18 09:28:29 -0300

    Unicode fix, coding style issues

--- wikilegis/notification/models.py
+++ wikilegis/notification/models.py
@@ -1,13 +1,10 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
 from datetime import datetime
 from django.db import models
 from django.utils.translation import ugettext_lazy as _
 
 from wikilegis.core.models import TimestampedMixin
-
-PERIODICITY_CHOICES = (
-    ('daily', _('Daily')),
-    ('weekly', _('Weekly'))
-)
 
 
 class HistoryNotification(models.Model):
@@ -24,6 +21,11 @@ class HistoryNotification(models.Model):
 
 
 class Newsletter(TimestampedMixin):
+    PERIODICITY_CHOICES = (
+        ('daily', _('Daily')),
+        ('weekly', _('Weekly'))
+    )
+
     bill = models.ForeignKey('core.Bill', verbose_name=_('bill'))
     user = models.ForeignKey('auth2.User', verbose_name=_('user'))
     periodicity = models.CharField(_('periodicity'), max_length=20, choices=PERIODICITY_CHOICES, default='daily')
@@ -35,4 +37,4 @@ class Newsletter(TimestampedMixin):
         unique_together = ('user', 'bill')
 
     def __unicode__(self):
-        return '%s - %s' % (unicode(self.user), unicode(self.bill.title))
+        return '{user} - {bill}'.format(user=self.user, bill=self.bill)

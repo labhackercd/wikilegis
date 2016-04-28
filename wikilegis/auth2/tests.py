b145d86e35bf06df2cd87779b32d783aedc49522
Ruyther Costa <ruy_fusion@hotmail.com>
2016-04-28 17:45:08 -0300

    Test for the login url

--- wikilegis/auth2/tests.py
+++ wikilegis/auth2/tests.py
@@ -1,3 +1,11 @@
 from django.test import TestCase
+from templatetags import auth2
+import models
+from django.test.client import Client
 
-# Create your tests here.
+
+class TestUserLogin(TestCase):
+    def test_is_account_login_page(self):
+        client = Client()
+        response = client.get('/accounts/login/')
+        self.failUnlessEqual(response.status_code, 200)

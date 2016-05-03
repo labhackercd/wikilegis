b98aa56e7b617aff86e06a78529e7658df8bae07
tiagorc <tiagobrasil2005@yahoo.com.br>
2016-05-03 17:21:28 -0300

    admin.py conforms with PEP8

--- wikilegis/auth2/admin.py
+++ wikilegis/auth2/admin.py
@@ -9,8 +9,12 @@ from image_cropping import ImageCroppingMixin
 class UserAdmin(BaseUserAdmin, ImageCroppingMixin, admin.ModelAdmin):
     fieldsets = (
         (None, {'fields': ('email', 'password')}),
-        (_('Personal info'), {'fields': ('first_name', 'last_name', 'avatar', 'cropping')}),
-        (_('Permissions'), {'fields': ('is_active', 'is_staff', 'is_superuser', 'groups', 'user_permissions')}),
+        (_('Personal info'), {'fields': (
+                                        'first_name', 'last_name',
+                                        'avatar', 'cropping')}),
+        (_('Permissions'), {'fields': (
+                                    'is_active', 'is_staff', 'is_superuser',
+                                    'groups', 'user_permissions')}),
         (_('Important dates'), {'fields': ('last_login', 'date_joined')}),
     )
     add_fieldsets = (

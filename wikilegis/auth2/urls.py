9eb8726a60bedf2985b3df3959be8f9033eb0daa
tiagorc <tiagobrasil2005@yahoo.com.br>
2016-05-03 17:44:52 -0300

    urls.py conforms to PEP8

--- wikilegis/auth2/urls.py
+++ wikilegis/auth2/urls.py
@@ -1,17 +1,27 @@
 from django.conf import settings
 from django.conf.urls import url, include
 from registration.backends.default.views import RegistrationView
-from registration.backends.simple.views import RegistrationView as SimpleRegistrationView
+from registration.backends.simple.views import (
+                                    RegistrationView as SimpleRegistrationView)
 from wikilegis.auth2.views import ActivationCompleteView
 
 urlpatterns = [
-    # XXX We want the user to be redirected after successful account activation.
-    # So we override the `registration_activation_complete` view to do the redirect.
-    # Note that tt overrides the included view from `registration.backends.default`
-    # and should always come before its inclusion or else little cutie puppies will cry.
-    url(r'^activate/complete/$', ActivationCompleteView.as_view(),
+    # XXX We want the user to be redirected after successful account activation
+    # So we override the `registration_activation_complete`
+    #   view to do the redirect.
+    # Note that tt overrides the included
+    #   view from `registration.backends.default`
+    # and should always come before its inclusion or else little cutie puppies
+    #   will cry.
+
+    url(
+        r'^activate/complete/$',
+        ActivationCompleteView.as_view(),
         name='registration_activation_complete'),
-    url(r'^activate/resend/$', 'wikilegis.auth2.views.resend_activation', name='resend_activation'),
+    url(
+        r'^activate/resend/$',
+        'wikilegis.auth2.views.resend_activation',
+        name='resend_activation'),
     url(r'^', include('registration.backends.default.urls')),
 ]
 
@@ -22,7 +32,16 @@ else:
     registration_view = SimpleRegistrationView
 
 urlpatterns += [
-    url(r'^register/$', registration_view.as_view(), name='registration_register'),
-    url(r'^edit/$', 'wikilegis.auth2.views.edit_profile', name='edit_profile'),
-    url(r'^users/(?P<user_id>\d+)/$', 'wikilegis.auth2.views.show_users_profile', name='users_profile'),
+    url(
+        r'^register/$',
+        registration_view.as_view(),
+        name='registration_register'),
+    url(
+        r'^edit/$',
+        'wikilegis.auth2.views.edit_profile',
+        name='edit_profile'),
+    url(
+        r'^users/(?P<user_id>\d+)/$',
+        'wikilegis.auth2.views.show_users_profile',
+        name='users_profile'),
 ]

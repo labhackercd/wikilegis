954a797c71865f0c6636afa9e40b3e2472a91934
MaxOliveira <maxwell_oliveira2@hotmail.com>
2016-05-04 20:53:14 -0300

    send_mail.py conforms PEP8

--- wikilegis/notification/management/commands/send_mail.py
+++ wikilegis/notification/management/commands/send_mail.py
@@ -8,7 +8,7 @@ from django.core.mail import EmailMultiAlternatives
 from django.template.loader import render_to_string
 from django_comments.models import Comment
 from wikilegis.auth2.models import User
-from wikilegis.core.models import Bill, CitizenAmendment, UpDownVote, BillSegment
+from wikilegis.core.models import Bill, UpDownVote, BillSegment
 from wikilegis.notification.models import HistoryNotification
 
 
@@ -22,27 +22,44 @@ class Command(BaseCommand):
             top_amendments = []
             segment_ctype = ContentType.objects.get_for_model(BillSegment)
             for segment in bill.segments.filter(original=True):
-                import ipdb; ipdb.set_trace()
-                up_votes_segment = UpDownVote.objects.filter(content_type=segment_ctype, object_id=segment.id,
+                import ipdb
+                ipdb.set_trace()
+                up_votes_segment = UpDownVote.objects.filter(content_type=
+                                                             segment_ctype,
+                                                             object_id=
+                                                             segment.id,
                                                              vote=True).count()
-                down_votes_segment = UpDownVote.objects.filter(content_type=segment_ctype, object_id=segment.id,
-                                                               vote=False).count()
+                down_votes_segment = UpDownVote.objects.filter(content_type=
+                                                               segment_ctype,
+                                                               object_id=
+                                                               segment.id,
+                                                               vote=
+                                                               False).count()
                 score_segment = up_votes_segment - down_votes_segment
                 for amendment in segment.substitutes.all():
-                    votes_amendment = UpDownVote.objects.filter(content_type=segment_ctype, object_id=amendment.pk)
+                    votes_amendment = UpDownVote.objects.filter(content_type=
+                                                                segment_ctype,
+                                                                object_id=
+                                                                amendment.pk)
                     try:
-                        last_email = HistoryNotification.objects.get(amendment=amendment)
+                        last_email = HistoryNotification.objects.get(amendment=
+                                                                     amendment)
                         if votes_amendment:
-                            up_votes_amendment = votes_amendment.filter(vote=True).count()
-                            down_votes_amendment = votes_amendment.filter(vote=False).count()
+                            up_votes_amendment = votes_amendment.filter(
+                                vote=True).count()
+                            down_votes_amendment = votes_amendment.filter(
+                                vote=False).count()
                             score_amendment = up_votes_amendment - down_votes_amendment
                             last_vote = votes_amendment.latest('modified')
                             if last_email.hour < last_vote.modified:
                                 if score_amendment > score_segment:
                                     top_amendments.append(amendment.id)
-                        comments = Comment.objects.filter(object_pk=amendment.pk,
-                                                          content_type=segment_ctype,
-                                                          submit_date__gte=last_email.hour)
+                        comments = Comment.objects.filter(object_pk=
+                                                          amendment.pk,
+                                                          content_type=
+                                                          segment_ctype,
+                                                          submit_date__gte=
+                                                          last_email.hour)
                         if comments:
                             for comment in comments:
                                 amendment_comments[amendment].append(comment)
@@ -50,8 +67,10 @@ class Command(BaseCommand):
                             last_email.save()
                     except HistoryNotification.DoesNotExist:
                         segment_amendments[segment].append(amendment)
-                        comments = Comment.objects.filter(object_pk=amendment.pk,
-                                                          content_type=segment_ctype)
+                        comments = Comment.objects.filter(object_pk=
+                                                          amendment.pk,
+                                                          content_type=
+                                                          segment_ctype)
                         history = HistoryNotification()
                         history.amendment = amendment
                         history.hour = datetime.now()
@@ -61,8 +80,10 @@ class Command(BaseCommand):
                                 history.hour = datetime.now()
                         history.save()
                         if votes_amendment:
-                            up_votes_amendment = votes_amendment.filter(vote=True).count()
-                            down_votes_amendment = votes_amendment.filter(vote=False).count()
+                            up_votes_amendment = votes_amendment.filter(
+                                vote=True).count()
+                            down_votes_amendment = votes_amendment.filter(
+                                vote=False).count()
                             score_amendment = up_votes_amendment - down_votes_amendment
                             if score_amendment > score_segment:
                                 top_amendments.append(amendment.id)
@@ -76,10 +97,12 @@ class Command(BaseCommand):
                                          'amendments': dict(segment_amendments),
                                          'comments': dict(amendment_comments),
                                          'proposition': proposition,
-                                         'top_amendments': BillSegment.objects.filter(id__in=top_amendments)})
+                                         'top_amendments':
+                                         BillSegment.objects.filter(id__in=top_amendments)})
                 superusers = User.objects.filter(is_superuser=True)
                 email_list = []
-                subject = u'[Wikilegis] Atualizações ao %s %s' % (bill.title, proposition)
+                subject = u'[Wikilegis] Atualizações ao %s %s' % (
+                    bill.title, proposition)
                 for superuser in superusers:
                     email_list.append(superuser.email)
                 for editor in bill.editors.all():

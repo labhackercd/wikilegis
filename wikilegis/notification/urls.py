dee48bf450a27061439f66afdc2f759ca3c3ff69
MaxOliveira <maxwell_oliveira2@hotmail.com>
2016-05-04 19:32:44 -0300

    urls.py conforms PEP8

--- wikilegis/notification/urls.py
+++ wikilegis/notification/urls.py
@@ -2,5 +2,6 @@ from django.conf.urls import url
 
 urlpatterns = [
     url(r'^verify/(?P<bill_id>\d+)/$',
-        'wikilegis.notification.views.verify_newsletter', name='verify_newsletter'),
+        'wikilegis.notification.views.verify_newsletter',
+        name='verify_newsletter'),
 ]

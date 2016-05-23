3ff1788d9bca6bc4a12bcd99f38bea3011fc8926
Matheus Fernandes <matheus.souza.fernandes@gmail.com>
2016-05-23 16:37:14 -0300

    Updated API to show foreign keys id
    Signed-off-by: Matheus Fernandes <matheus.souza.fernandes@gmail.com>

--- wikilegis/core/serializers.py
+++ wikilegis/core/serializers.py
@@ -1,4 +1,3 @@
-from django.utils.text import slugify
 from django_comments.models import Comment
 from rest_framework import serializers
 
@@ -7,64 +6,33 @@ from wikilegis.core.models import Bill, BillSegment
 
 
 class SegmentSerializer(serializers.ModelSerializer):
-    parent = serializers.SerializerMethodField('get_parent_name')
-    type = serializers.SerializerMethodField('get_type_name')
 
     class Meta:
         model = BillSegment
-        fields = ('id', 'bill', 'original', 'replaced', 'parent', 'type', 'number', 'content')
-
-    def get_parent_name(self, obj):
-        if obj.parent:
-            if obj.parent.number:
-                if obj.parent.number == '0':
-                    return slugify(obj.parent.type.name)
-                else:
-                    return '{kind} {number}'.format(
-                        kind=slugify(obj.parent.type.name), number=obj.parent.number)
-            else:
-                return obj.parent
-
-    def get_type_name(self, obj):
-        return slugify(obj.type.name)
+        fields = ('id', 'bill', 'original', 'replaced', 'parent',
+                  'type', 'number', 'content')
 
 
 class BillSegmentSerializer(serializers.ModelSerializer):
-    queryset = BillSegment.objects.filter(original=True)
-    parent = serializers.SerializerMethodField('get_parent_name')
-    type = serializers.SerializerMethodField('get_type_name')
 
     class Meta:
         model = BillSegment
         fields = ('id', 'parent', 'type', 'number', 'content')
 
-    def get_parent_name(self, obj):
-        if obj.parent:
-            if obj.parent.number:
-                if obj.parent.number == '0':
-                    return slugify(obj.parent.type.name)
-                else:
-                    return '{kind} {number}'.format(
-                        kind=slugify(obj.parent.type.name), number=obj.parent.number)
-            else:
-                return obj.parent
-
-    def get_type_name(self, obj):
-        return slugify(obj.type.name)
-
 
 class BillSerializer(serializers.ModelSerializer):
-    segments = BillSegmentSerializer(many=True, read_only=True)
 
     class Meta:
         model = Bill
-        fields = ('id', 'title', 'epigraph', 'description', 'status', 'theme', 'segments')
+        fields = ('id', 'title', 'epigraph', 'description',
+                  'status', 'theme', 'segments')
 
 
 class CommentsSerializer(serializers.ModelSerializer):
     class Meta:
         model = Comment
-        fields = ('id', 'user', 'user_name', 'user_email', 'submit_date', 'content_type', 'object_pk', 'comment')
+        fields = ('id', 'user', 'user_name', 'user_email', 'submit_date',
+                  'content_type', 'object_pk', 'comment')
 
 
 class UserSerializer(serializers.ModelSerializer):

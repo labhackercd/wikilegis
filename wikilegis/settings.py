a3cc466ca8d778e02b92b64c9d631b2c2ae47397
Felipe Álvarez <falvarez@ciudadanointeligente.org>
2016-07-15 16:02:21 -0400

    Adding an import from local_settings.py if available

--- wikilegis/settings.py
+++ wikilegis/settings.py
@@ -291,3 +291,8 @@ EMAIL_PORT = config('EMAIL_PORT', cast=int, default=587)
 EMAIL_HOST_USER = config('EMAIL_HOST_USER', default='')
 EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD', default='')
 EMAIL_USE_TLS = config('EMAIL_USE_TLS', cast=bool, default=True)
+
+try:
+    from local_settings import *
+except ImportError:
+    pass

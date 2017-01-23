b2343f608d05d2918732a3dd983d93c195dba8e4
Matheus Fernandes <matheus.souza.fernandes@gmail.com>
2017-01-23 17:31:37 -0200

    Fixed template folder on settings
    Signed-off-by: Matheus Fernandes <matheus.souza.fernandes@gmail.com>

--- wikilegis/wikilegis/settings/staticfiles.py
+++ wikilegis/wikilegis/settings/staticfiles.py
@@ -61,7 +61,7 @@ CONTEXT_PROCESSORS += WIKILEGIS_CONTEXT_PROCESSORS
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'wikilegis', 'templates')],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': CONTEXT_PROCESSORS,

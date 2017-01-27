442df8fd769e215fc63712f4855c2fb2cde9e564
Cristiano Santos <cristianorsantos.95@gmail.com>
2017-01-27 11:57:13 -0200

    Rename bills-list to bill-list and create include

--- wikilegis/static/javascript/search.js
+++ wikilegis/static/javascript/search.js
@@ -2,10 +2,10 @@ import List from 'list.js';
 
 const options = {
   searchClass: 'banner__search',
-  listClass: 'wikilegis__bills-list',
+  listClass: 'wikilegis__bill-list',
   valueNames: ['item__title', 'item__text'],
 };
 
-const billsListSearch = new List('bills-list-search', options);
+const billsListSearch = new List('bill-list-search', options);
 
 export default billsListSearch;

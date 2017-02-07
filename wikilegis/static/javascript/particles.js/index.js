ee45d7ed3a6044145b891c5bbc8f2997afe51291
cfviotti <caio@viotti.me>
2017-02-07 15:30:49 -0200

    Fix particles.js path

--- wikilegis/static/javascript/particles.js/index.js
+++ wikilegis/static/javascript/particles.js/index.js
@@ -1,3 +1,3 @@
 import 'particles.js';
 
-particlesJS.load('particles-js', 'static/javascript/particles.js/settings.json'); // eslint-disable-line no-undef
+particlesJS.load('particles-js', '/static/javascript/particles.js/settings.json'); // eslint-disable-line no-undef

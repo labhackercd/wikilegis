fd665cf93e266f34533b9cad82d99da659c7323c
Matheus Fernandes <matheus.souza.fernandes@gmail.com>
2017-02-22 18:06:30 -0300

    Fixed eslint errors
    Signed-off-by: Matheus Fernandes <matheus.souza.fernandes@gmail.com>

--- wikilegis/static/javascript/content/modules/amendmentDiff.js
+++ wikilegis/static/javascript/content/modules/amendmentDiff.js
@@ -3,32 +3,32 @@ import $ from 'jquery';
 const diff = require('diff');
 
 function amendmentDiffModule() {
-    function updateDiff(inputEl) {
-        const containerEL = $(inputEl).closest('[data-segment-content]')[0]
-        const segmentContent = containerEL.dataset.segmentContent;
-        const diffEl = buildMarkup(diff.diffWordsWithSpace(segmentContent, inputEl.value));
+  function buildMarkup(oldText, newText) {
+    const diffElement = document.createDocumentFragment();
+    diff.diffWordsWithSpace(oldText, newText).forEach((part) => {
+      let className = '';
+      if (part.added) className = 'diff-added';
+      else if (part.removed) className = 'diff-removed';
 
-        const diffWrapperEl = containerEL.querySelector('[data-diff-wrapper]');
-        diffWrapperEl.innerHTML = '';
-        diffWrapperEl.appendChild(diffEl);
-    }
+      const span = document.createElement('span');
+      $(span).addClass(className);
+      span.appendChild(document.createTextNode(part.value));
+      diffElement.appendChild(span);
+    });
+    return diffElement;
+  }
 
-    function buildMarkup(diff) {
-        let diffElement = document.createDocumentFragment();
-        diff.forEach((part) => {
-            let className = '';
-            if (part.added) className = 'diff-added';
-            else if (part.removed)  className = 'diff-removed';
+  function updateDiff(inputEl) {
+    const containerEL = $(inputEl).closest('[data-segment-content]')[0];
+    const segmentContent = containerEL.dataset.segmentContent;
+    const diffEl = buildMarkup(segmentContent, inputEl.value);
 
-            let span = document.createElement('span');
-            $(span).addClass(className);
-            span.appendChild(document.createTextNode(part.value));
-            diffElement.appendChild(span);
-        })
-        return diffElement;
-    }
+    const diffWrapperEl = containerEL.querySelector('[data-diff-wrapper]');
+    diffWrapperEl.innerHTML = '';
+    diffWrapperEl.appendChild(diffEl);
+  }
 
-    return { updateDiff };
+  return { updateDiff };
 }
 
 export default amendmentDiffModule;

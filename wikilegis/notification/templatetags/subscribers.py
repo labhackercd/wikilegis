fc36a8b23d7b599befd9efc88ae3c7abd14e0336
erivanio <erivanio.vanin@gmail.com>
2017-03-03 10:39:50 -0300

    rafactor template tags in notification

---
+++ wikilegis/notification/templatetags/subscribers.py
@@ -0,0 +1,14 @@
+from django.core.exceptions import ObjectDoesNotExist
+from django.template import Library
+from notification.models import Newsletter
+
+register = Library()
+
+
+@register.assignment_tag()
+def subscriber(bill_id, user_id):
+    try:
+        newsletter = Newsletter.objects.get(bill_id=bill_id, user_id=user_id)
+        return newsletter.status
+    except ObjectDoesNotExist:
+        return False

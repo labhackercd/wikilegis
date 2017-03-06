1e140fb28f0516483c4a2b1ae655b2e566c9a2d1
Matheus Fernandes <matheus.souza.fernandes@gmail.com>
2017-03-06 11:30:26 -0300

    Added segment type to additive amendment preview
    Signed-off-by: Matheus Fernandes <matheus.souza.fernandes@gmail.com>

--- wikilegis/static/javascript/content/modules/preview.js
+++ wikilegis/static/javascript/content/modules/preview.js
@@ -4,7 +4,10 @@ function previewModule() {
   function additiveAmendmentPreview(inputEl) {
     const tabWrapperEl = $(inputEl).closest('[data-tab-container]')[0];
     const previewWrapperEl = tabWrapperEl.querySelector('[data-additive-preview]');
-    previewWrapperEl.innerText = inputEl.value;
+    const typeSelectEl = inputEl.previousElementSibling;
+    let typeText = typeSelectEl.options[typeSelectEl.selectedIndex].innerText;
+    if (!inputEl.value) typeText = '';
+    previewWrapperEl.innerHTML = `<strong>${typeText}</strong> ${inputEl.value}`;
   }
 
   return { additiveAmendmentPreview };

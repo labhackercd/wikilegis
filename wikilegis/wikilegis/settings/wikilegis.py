3bb539f856f5b134e86fc6344f431439902c5f2c
Matheus Fernandes <matheus.souza.fernandes@gmail.com>
2017-03-09 18:26:45 -0300

    Added plugins settings_variables to django settings
    Signed-off-by: Matheus Fernandes <matheus.souza.fernandes@gmail.com>

--- wikilegis/wikilegis/settings/wikilegis.py
+++ wikilegis/wikilegis/settings/wikilegis.py
@@ -13,3 +13,13 @@ for module_loader, name, ispkg in pkgutil.iter_modules([pkgpath]):
     module = import_module('..' + name, package=__name__)
     for var in dir(module):
         variables[var] = module.__dict__[var]
+
+from core import plugins # noqa
+
+plugins_dict = plugins.load_current_plugins()
+
+for name, is_active in plugins_dict.items():
+    if is_active:
+        plugin_settings = plugins.get_settings(name)
+        for key, value in plugin_settings.SETTINGS_VARIABLES.items():
+            variables[key] = value

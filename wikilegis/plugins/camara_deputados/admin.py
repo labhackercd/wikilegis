67ca261d1733b5dc2b36257828ac73750b1650d9
Matheus Fernandes <matheus.souza.fernandes@gmail.com>
2017-03-10 14:05:31 -0300

    Added admin to billinfo and proposaltype
    Signed-off-by: Matheus Fernandes <matheus.souza.fernandes@gmail.com>

--- wikilegis/plugins/camara_deputados/admin.py
+++ wikilegis/plugins/camara_deputados/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
+from plugins.camara_deputados import models
 
-# Register your models here.
+
+class BillInfoAdmin(admin.ModelAdmin):
+    list_display = ('bill', 'proposal_type', 'proposal_number',
+                    'proposal_year', 'situation')
+    readonly_fields = ('author',)
+
+
+class ProposalTypeAdmin(admin.ModelAdmin):
+    list_display = ('initials', 'description')
+    list_filter = ('initials',)
+    search_fields = ('initials',)
+
+
+admin.site.register(models.ProposalType, ProposalTypeAdmin)
+admin.site.register(models.BillInfo, BillInfoAdmin)

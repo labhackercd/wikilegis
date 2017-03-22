0c92fd9892e4d50d16b1842dcdd0fdaf35f2da26
Matheus Fernandes <matheus.souza.fernandes@gmail.com>
2017-03-22 11:14:23 -0300

    Fixed wikileigs settings on wsgi.py
    Signed-off-by: Matheus Fernandes <matheus.souza.fernandes@gmail.com>

--- wikilegis/wikilegis/wsgi.py
+++ wikilegis/wikilegis/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "wikilegis.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "wikilegis.settings.wikilegis")
 
 application = get_wsgi_application()

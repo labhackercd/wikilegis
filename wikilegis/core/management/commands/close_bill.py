6ef4b0d18c7ad22a181a406e731790b556c2a585
erivanio <erivanio.vanin@gmail.com>
2017-04-04 16:20:17 -0300

    add cron to close bill

---
+++ wikilegis/core/management/commands/close_bill.py
@@ -0,0 +1,12 @@
+from django.core.management.base import BaseCommand
+from core.models import Bill
+from datetime import date
+
+
+class Command(BaseCommand):
+    def handle(self, *args, **options):
+        bills = Bill.objects.filter(status="published",
+                                    closing_date__lt=date.today())
+        for bill in bills:
+            bill.status = 'closed'
+            bill.save()

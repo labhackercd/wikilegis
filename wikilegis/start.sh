13e270b5ded2fca37a9c1707586bb3a7e4d51213
Matheus Fernandes <matheus.souza.fernandes@gmail.com>
2017-05-22 14:27:02 -0300

    Added compress and config.py to start.sh
    Signed-off-by: Matheus Fernandes <matheus.souza.fernandes@gmail.com>

--- wikilegis/start.sh
+++ wikilegis/start.sh
@@ -1,16 +1,11 @@
 #!/bin/bash
 WIKILEGIS_DIR="/var/labhacker/wikilegis"
 
+python config.py
 python manage.py bower_install --allow-root
+python manage.py compress --force
 python manage.py collectstatic --no-input
 python manage.py compilemessages
-
-# Seta um novo diretório foi passado como raiz para o SAPL
-# caso esse tenha sido passado como parâmetro
-if [ "$1" ]
-then
-    WIKILEGIS_DIR="$1"
-fi
 
 NAME="Wikilegis"
 DJANGODIR=/var/labhacker/wikilegis/wikilegis/
@@ -18,7 +13,6 @@ USER=`whoami`
 GROUP=`whoami`
 NUM_WORKERS=9
 DJANGO_WSGI_MODULE=wikilegis.wsgi
-
 
 # Start your Django Unicorn
 # Programs meant to be run under supervisor should not daemonize themselves (do not use --daemon)
